7dcc9a5c9e57b28611d1a3a77244c7d10145f37f
Kacper Polak <kacper@hypequality.com>
2018-07-11 23:28:37 +0200

    add pref jsdoc

--- src/lib/interfaces/pref.ts
+++ src/lib/interfaces/pref.ts
@@ -1,10 +1,77 @@
+/**
+ * Preference
+ *
+ * @export
+ * @interface Pref
+ */
 export interface Pref {
-  id: number;
-  app_id: number;
-  key: string;
-  value: string;
-  created_at: string;
-  created_by: number;
-  updated_at: string;
-  updated_by: number;
+
+  /**
+   * Id of preference
+   *
+   * @readonly
+   * @type {number}
+   * @memberof Pref
+   */
+  readonly id?: number;
+
+  /**
+   * Application id
+   *
+   * @type {number}
+   * @memberof Pref
+   */
+  app_id?: number;
+
+  /**
+   * Key of preference
+   *
+   * @type {string}
+   * @memberof Pref
+   */
+  key?: string;
+
+  /**
+   * Value of preference
+   *
+   * @type {string}
+   * @memberof Pref
+   */
+  value?: string;
+
+  /**
+   * Created at
+   *
+   * @readonly
+   * @type {string}
+   * @memberof Pref
+   */
+  readonly created_at?: string;
+
+  /**
+   * Created by
+   *
+   * @readonly
+   * @type {string}
+   * @memberof Pref
+   */
+  readonly created_by?: number;
+
+  /**
+   * Updated at
+   *
+   * @readonly
+   * @type {string}
+   * @memberof Pref
+   */
+  readonly updated_at?: string;
+
+  /**
+   * Updated by
+   *
+   * @readonly
+   * @type {string}
+   * @memberof Pref
+   */
+  readonly updated_by?: number;
 }
