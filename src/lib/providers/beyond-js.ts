492f5cc6aee85a4b37355cefebc6b2e060412025
Kacper Polak <kacper@hypequality.com>
2018-07-11 23:02:17 +0200

    comment provider with internal tag

--- src/lib/providers/beyond-js.ts
+++ src/lib/providers/beyond-js.ts
@@ -2,8 +2,15 @@ import { InjectionToken } from '@angular/core';
 import * as Beyond from '@getbeyond/beyond-js';
 import * as _ from 'lodash';
 
+/**
+ * @internal
+ */
 export const BeyondJS = new InjectionToken<Beyond>('beyond');
 
+/**
+ * @internal
+ * @returns {Beyond}
+ */
 export function BeyondJSProviderFactory(): Beyond {
   // Use window.beyond if it exists first
   // If not, then create, because we always want only one BeyondJS instance per window, and return it
@@ -13,6 +20,9 @@ export function BeyondJSProviderFactory(): Beyond {
   return (<any>window).beyond;
 }
 
+/**
+ * @internal
+ */
 export const BeyondJSProvider = {
   provide: BeyondJS,
   useFactory: BeyondJSProviderFactory
